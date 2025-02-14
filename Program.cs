794c6746cd8db4eae3de9b4d74e147e29ed50a15
Kumi912 <flor.maribel999@gmail.com>
2025-02-13 20:12:16 -0600

    Inicio

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese la cantidad de salario de un dia");
+        double salario = double.Parse(Console.ReadLine());
+
+        Aguinaldo aguinaldo = new Aguinaldo(salario);
+
+        Console.WriteLine("El aguinaldo es " + aguinaldo.CalcularAguinaldo());
+    }
+
+    public class Aguinaldo
+    {
+        public double Salario { get; set; }
+
+        public Aguinaldo(double salario)
+        {
+            Salario = salario;
+        }
+
+        public double CalcularAguinaldo()
+        {
+            return Salario * 15;
+        }
+    }
+}
